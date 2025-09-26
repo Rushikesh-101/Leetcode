3577805e3f4a83b36f3082cc6843b223f5d3ebcd
Rushikesh-101 <161769245+Rushikesh-101@users.noreply.github.com>
2025-09-27 02:55:57 +0530

    Time: 159 ms (71.23%), Space: 31 MB (79.36%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,62 +1,28 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         
-        # track index range using k
 
-        # traverse nums using for
-            #once at end of curr window : calc max( ie the front element in que)
-        que = deque()
-        front = 0
-        rear = 0
-        a = 0
-        b = 2
+
+        # keep queue[0] updated as per window 
+        # compare i and queue[-1] then add
+        # from k-1, start appending to res
+
+
+        
         res = []
-        ctr = 0
-        itr = 0
-        for index in range(len(nums)):
-            ctr += 1
+        que = deque()
 
-            # this pops indexes from que that are out of current window
-            while que and que[0] < a:
+        for i in range(len(nums)): # i = indexes of nums : 0 1 2 3
+            
+            if que and que[0] <= i-k : 
                 que.popleft()
-
-            if not que:
-                que.append(index)
-            else:
-                while que and nums[index] > nums[que[-1]]:
-                    que.pop()
-                que.append(index)
             
+            while que and nums[que[-1]] < nums[i] :
+                que.pop()
+            que.append(i)
 
-
-            
-            if ctr >= k:
+            if i - k >= -1 :
                 res.append(nums[que[0]])
-
-            if ctr >= k:
-                a +=1
-                b +=1
-
-
             
-            
-                
-
         return res
-                
-            
 
-
-
-        #initiate deque with F = 0 and rear = -1
-
-        # if que empty direct at rear
-
-        # for next elements compare with rear. 
-            # while grt than rear. pop rear
-
-        # when calc max : while front is out of window. pop front
-
-
-        
-        
