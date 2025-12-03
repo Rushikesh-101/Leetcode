b9557893b842c4bf67ad99158491ffe5aac826a5
Rushikesh Mokashi <161769245+Rushikesh-101@users.noreply.github.com>
2025-12-03 14:12:48 +0530

    Time: 421 ms (5.12%), Space: 17.7 MB (40.65%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -9,3 +9,33 @@ class Solution:
             return x
         
         return recurr(n)
+
+        '''
+
+        Above code recomputes value :
+        Ex : Your Fibonacci recursion calls the same values repeatedly:
+
+            fib(5) calls fib(4) and fib(3)
+            fib(4) calls fib(3) and fib(2)
+            fib(3) calls fib(2) and fib(1)
+            ...and so on
+            You end up recomputing fib(3) multiple times. 
+
+        Instead store computed values in dict, check if resusable.
+
+        '''
+        storage = {}
+
+        def recurr(x):
+            if x in storage:
+                return storage[n]
+
+            if x == 0 or x == 1:
+                return 0 if x==0 else 1
+
+            result = recurr(x-1)+recurr(x-2)
+            storage[x] = result
+            return result
+        
+        return recurr(n)
+        
