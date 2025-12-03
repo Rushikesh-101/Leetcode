f9d6de609b540814bcd339398f20d0c4113a7d22
Rushikesh Mokashi <161769245+Rushikesh-101@users.noreply.github.com>
2025-12-03 14:05:19 +0530

    Time: 410 ms (5.12%), Space: 17.8 MB (40.65%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -2,12 +2,10 @@ class Solution:
     def fib(self, n: int) -> int:
         
         def recurr(x):
-            if x > 1:
-                x = recurr(x-1)+recurr(x-2)
-            elif x == 0:
-                return 0
-            elif x == 1:
-                return 1
+            if x == 0 or x == 1:
+                return 0 if x==0 else 1
+
+            x = recurr(x-1)+recurr(x-2)
             return x
         
         return recurr(n)
