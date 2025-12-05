27d8ff588603aeb7d3498d9cf65222231d57cdb2
Rushikesh Mokashi <161769245+Rushikesh-101@users.noreply.github.com>
2025-12-05 13:06:30 +0530

    Time: 0 ms (100%), Space: 18.9 MB (96.01%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -7,10 +7,6 @@
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         
-        '''
-        Queue based solution : 
-
-
         if root :
          
             que = deque([root]) # [root] brackets cause root isnt iterative
@@ -28,25 +24,5 @@ class Solution:
             return depth
         else :
             return 0
-        '''
 
         
-    # Recursive solution :
-
-        def depth(node):
-            # If node, called function, upon doesnt exist,returns 0 height value
-            if not node:
-                return 0
-
-            # Calls th function on right and left child if they exist 
-            right = depth(node.right)
-            left = depth(node.left)
-            
-            # When returning value from node A , we return maximum of it right child and left child height,
-            # +1 of itself
-            return 1 + max(left,right) 
-        
-        return depth(root)
-            
-
-    
