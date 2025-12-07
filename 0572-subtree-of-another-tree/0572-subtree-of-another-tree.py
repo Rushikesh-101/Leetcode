95f26aa114e3c265b0f89084a00fd0dd1d259452
Rushikesh Mokashi <161769245+Rushikesh-101@users.noreply.github.com>
2025-12-07 15:02:38 +0530

    Time: 43 ms (61.83%), Space: 18 MB (59.41%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -8,75 +8,49 @@ class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
        
 
-            # # using 2 functions :
-            #     # 1st to find potential matching roots
-            #     # 2nd to confirm sub tree similarity
+            # using 2 functions :
+                # 1st to find potential matching roots
+                # 2nd to confirm sub tree similarity
 
 
 
-            # def check(node,subnode):
-            #     if not node and not subnode:
-            #         return True
-            #     if not node or not subnode:
-            #         return False
-            #     if node.val != subnode.val:
-            #         return False
-                
-            #     leftval = check(node.left,subnode.left)
-                
-            #     rightval = check(node.right,subnode.right)
-
-            #     if leftval and rightval:
-            #         return True
-
-
-                
-                
-
-
-
-
-            
-            # def subtree(node,subRoot):
-                
-            #     if node.val == subRoot.val:
-            #         print("was called")
-            #         # call crosschecking function
-            #         if check(node,subRoot) == True:
-            #             print("went to another function")
-            #             return True
-
-            #     if not node.left and not node.right:
-            #         print("returned from here")
-            #         return 
-            #     else:
-            #         print("entered")                    
-            #         if node.left and subtree(node.left,subRoot) :
-            #             return True                   
-            #         if node.right and subtree(node.right,subRoot) :
-            #             return True
-                        
-            
-            # if subtree(root,subRoot) != True:
-            #     return False
-            # else:
-            #     return True
-            def isSubtree(root, subRoot):  
-                def isSame(a, b):
-                    if not a and not b:
-                        return True
-                    if not a or not b:
-                        return False
-                    if a.val != b.val:
-                        return False
-                    return isSame(a.left, b.left) and isSame(a.right, b.right)
-
-                if not root:
+            def check(node,subnode):
+                if not node and not subnode:
+                    return True
+                if not node or not subnode:
                     return False
+                if node.val != subnode.val:
+                    return False
+                
+                leftval = check(node.left,subnode.left)
+                
+                rightval = check(node.right,subnode.right)
 
-                if isSame(root, subRoot):
+                if leftval and rightval:
                     return True
 
-                return isSubtree(root.left, subRoot) or isSubtree(root.right, subRoot)
             
-            return isSubtree(root,subRoot)
+            def subtree(node,subRoot):
+                
+                if node.val == subRoot.val:                   
+                    if check(node,subRoot) == True:
+                        return True
+
+                if not node.left and not node.right:
+                    return 
+
+                else:                    
+                    if node.left and subtree(node.left,subRoot) :
+                        return True                   
+                    if node.right and subtree(node.right,subRoot) :
+                        return True
+
+
+
+
+
+            if subtree(root,subRoot) != True:
+                return False
+            else:
+                return True
+            
