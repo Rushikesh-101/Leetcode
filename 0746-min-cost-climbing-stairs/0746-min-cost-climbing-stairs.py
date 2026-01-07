5657b237f11f393b3567202cd8929769658803aa
Rushikesh Mokashi <161769245+Rushikesh-101@users.noreply.github.com>
2026-01-07 18:17:37 +0530

    Time: 0 ms (100%), Space: 19.2 MB (15.4%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,52 +1,17 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
 
-        # dp = {
-        #     (len(cost)-1) : cost[len(cost)-1],
-        #     (len(cost)-2) : cost[len(cost)-2]
-        # }
-
-        # def climb(step):
-        #     if step > len(cost)-1:
-        #         return 0
-        #     if (step) in dp:
-        #         return dp[(step)]
-
-        #     else :
-        #         cost_1 = climb(step+1)
-        #         cost_2 = climb(step+2)
-        #         dp[(step)] = min(cost_1,cost_2) + cost[step]
-        #         return min(cost_1,cost_2) + cost[step]
         
-        # return min(climb(0),climb(1))
-
-        # def climb():
-        #     dp = {
-        #         (len(cost)-1) : cost[len(cost)-1],
-        #         (len(cost)-2) : cost[len(cost)-2]
-        #     }
-        #     for i in range(len(cost)-3,-1,-1):
-        #         cost_i = min( dp[(i+1)], dp[(i+2)] ) + cost[i]
-        #         print("\n for index ",i," val saved ", min(cost[i+1],cost[i+2]), cost[i])
-        #         dp[(i)] = cost_i
-            
-        #     print(dp)
-        #     return min(dp[(0)],dp[(1)]) 
-
-        # return climb()
-
         def climb():
-
+            last_2 = 0
             last_1 = 0
-            last_2 = cost[0]
 
-            for i in range(1,len(cost)):
-
-                curr_cost = cost[i] + min(last_1,last_2)
-                last_1 = last_2
-                last_2 = curr_cost
+            for i in range(2,len(cost)+1):
+                curr_cost = min((cost[i-1])+ last_1, (cost[i-2])+last_2)
+                last_2 = last_1
+                last_1 = curr_cost
             
-            return min(curr_cost,last_1)
+            return curr_cost
         return climb()
-
+                
 
