3e7915f5fefb77f2f220aadd4be4544e8f0affdc
Rushikesh Mokashi <161769245+Rushikesh-101@users.noreply.github.com>
2026-01-07 17:57:29 +0530

    Time: 3 ms (73.3%), Space: 19.4 MB (14.9%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -19,17 +19,34 @@ class Solution:
         #         return min(cost_1,cost_2) + cost[step]
         
         # return min(climb(0),climb(1))
-        def climb():
-            dp = {
-                (len(cost)-1) : cost[len(cost)-1],
-                (len(cost)-2) : cost[len(cost)-2]
-            }
-            for i in range(len(cost)-3,-1,-1):
-                cost_i = min( dp[(i+1)], dp[(i+2)] ) + cost[i]
-                print("\n for index ",i," val saved ", min(cost[i+1],cost[i+2]), cost[i])
-                dp[(i)] = cost_i
-            
-            print(dp)
-            return min(dp[(0)],dp[(1)]) 
 
+        # def climb():
+        #     dp = {
+        #         (len(cost)-1) : cost[len(cost)-1],
+        #         (len(cost)-2) : cost[len(cost)-2]
+        #     }
+        #     for i in range(len(cost)-3,-1,-1):
+        #         cost_i = min( dp[(i+1)], dp[(i+2)] ) + cost[i]
+        #         print("\n for index ",i," val saved ", min(cost[i+1],cost[i+2]), cost[i])
+        #         dp[(i)] = cost_i
+            
+        #     print(dp)
+        #     return min(dp[(0)],dp[(1)]) 
+
+        # return climb()
+
+        def climb():
+
+            last_1 = 0
+            last_2 = cost[0]
+
+            for i in range(1,len(cost)):
+
+                curr_cost = cost[i] + min(last_1,last_2)
+                last_1 = last_2
+                last_2 = curr_cost
+            
+            return min(curr_cost,last_1)
         return climb()
+
+
