1d568d98bc5b962970252db3825b2f362dd7c717
Rushikesh Mokashi <161769245+Rushikesh-101@users.noreply.github.com>
2026-01-07 16:38:28 +0530

    Time: 26 ms (5.36%), Space: 19.6 MB (13.76%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,22 +1,35 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
 
-        dp = {
-            str(len(cost)-1) : cost[len(cost)-1],
-            str(len(cost)-2) : cost[len(cost)-2]
-        }
+        # dp = {
+        #     str(len(cost)-1) : cost[len(cost)-1],
+        #     str(len(cost)-2) : cost[len(cost)-2]
+        # }
 
-        def climb(step):
-            if step > len(cost)-1:
-                return 0
-            if str(step) in dp:
-                return dp[str(step)]
+        # def climb(step):
+        #     if step > len(cost)-1:
+        #         return 0
+        #     if str(step) in dp:
+        #         return dp[str(step)]
 
-            else :
-                cost_1 = climb(step+1)
-                cost_2 = climb(step+2)
-                dp[str(step)] = min(cost_1,cost_2) + cost[step]
-                return min(cost_1,cost_2) + cost[step]
+        #     else :
+        #         cost_1 = climb(step+1)
+        #         cost_2 = climb(step+2)
+        #         dp[str(step)] = min(cost_1,cost_2) + cost[step]
+        #         return min(cost_1,cost_2) + cost[step]
         
-        return min(climb(0),climb(1))
+        # return min(climb(0),climb(1))
+        def climb():
+            dp = {
+                str(len(cost)-1) : cost[len(cost)-1],
+                str(len(cost)-2) : cost[len(cost)-2]
+            }
+            for i in range(len(cost)-3,-1,-1):
+                cost_i = min( dp[str(i+1)], dp[str(i+2)] ) + cost[i]
+                print("\n for index ",i," val saved ", min(cost[i+1],cost[i+2]), cost[i])
+                dp[str(i)] = cost_i
+            
+            print(dp)
+            return min(dp[str(0)],dp[str(1)]) 
 
+        return climb()
