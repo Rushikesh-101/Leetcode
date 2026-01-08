afe6a8d212afc7f70f240e21dd7d5901c77c219c
Rushikesh Mokashi <161769245+Rushikesh-101@users.noreply.github.com>
2026-01-08 16:50:44 +0530

    Time: 0 ms (100%), Space: 19.3 MB (9.14%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,33 +1,52 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        if len(nums) == 1:
-            return nums[0]
+        # if not nums:
+        #     return 0
+        # if len(nums) == 1:
+        #     return nums[0]
 
-            
+
         # In DP store indexes and the max value they can provide ahead of them.
-        dp = {}
-        def maxi(i):
-            if i in dp:
-                return dp[i]
-            if i+2 > len(nums)-1 :
-                dp[i] = nums[i]
-                print("\n result for idx",i,"is :",nums[i])
-                return nums[i]
+        # dp = {}
+        # def maxi(i):
+        #     if i in dp:
+        #         return dp[i]
+        #     if i+2 > len(nums)-1 :
+        #         dp[i] = nums[i]
+        #         return nums[i]
             
+        #     else:
+        #         grtst = 0
+        #         for idx in range(i+2,len(nums)):
+        #             sum = 0
+        #             sum += maxi(idx)
+        #             if sum > grtst:
+        #                 grtst = sum
+
+        #         result = grtst + nums[i]
+        #         dp[i] = result
+        #         print("\n result for idx",i,"is :",result)
+        #         return result
+
+        # return max(maxi(0),maxi(1))
+
+        if len(nums) < 3:
+            if len(nums) == 2 :
+                return max(nums[0],nums[1])
             else:
-                grtst = 0
-                for idx in range(i+2,len(nums)):
-                    sum = 0
-                    sum += maxi(idx)
-                    if sum > grtst:
-                        grtst = sum
+                return nums[0]
 
-                result = grtst + nums[i]
-                dp[i] = result
-                print("\n result for idx",i,"is :",result)
-                return result
+        def rob():
+            last_1 = max(nums[0],nums[1])
+            last_2 = nums[0]
 
-        return max(maxi(0),maxi(1))
+            for i in range(2, len(nums)):
+                curr_val = max(nums[i]+last_2, last_1)
+                last_2 = last_1
+                last_1 = curr_val
+
+            return curr_val
+        
+        return rob()
+
 
