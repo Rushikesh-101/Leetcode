e0b9d4ccdb3244258ceb27f4522080820de72aac
Rushikesh Mokashi <161769245+Rushikesh-101@users.noreply.github.com>
2026-01-12 03:09:07 +0530

    Time: 1607 ms (5.01%), Space: 497 MB (5.01%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -5,28 +5,56 @@ class Solution:
         '''
         DP = {}
         def demoni(x, amt):
-           
-            
+            if (x,amt) in DP:
+                return DP[(x,amt)]
+
+            if x == len(coins):
+                return float('inf')
             if amt == 0 :
                 return 0
             if amt < 0:
                 return float('inf')
 
-            for i in range(x,len(coins)):
-                if (i, amt) in DP:
-                    return DP[(i, amt)]
-                # not take :
-                result = demoni(i+1,amt)
+            
 
-                if coins[i] <= amt:
-                    result = min(result, demoni(x,amt-coins[i]) + 1)
-                DP[(x, amt)] = result
-                return result
-            return float('inf')
+            result = demoni(x+1,amt)
+            if coins[x] <= amt :
+                result = min(result, demoni(x,amt-coins[x])+1)
+            
+            DP[(x,amt)] = result
+            return result
 
         
+
+
+
         result = demoni(0, amount)
         if result == float('inf'):
             return -1
         else:
             return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # for i in range(x,len(coins)):
+        #         if (i, amt) in DP:
+        #             return DP[(i, amt)]
+        #         # not take :
+        #         result = demoni(i+1,amt)
+
+        #         if coins[i] <= amt:
+        #             result = min(result, demoni(x,amt-coins[i]) + 1)
+        #         DP[(x, amt)] = result
+        #         return result
+        #     return float('inf')
