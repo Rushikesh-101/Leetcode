902fb573a90f1c61a7b5e1f34df950b17b0aea81
Rushikesh Mokashi <161769245+Rushikesh-101@users.noreply.github.com>
2026-01-12 18:21:58 +0530

    Time: 458 ms (95.13%), Space: 19.6 MB (33.82%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -3,58 +3,64 @@ class Solution:
         '''
         DP [coins to choose from][amount remaining]
         '''
-        DP = {}
-        def demoni(x, amt):
-            if (x,amt) in DP:
-                return DP[(x,amt)]
+        # DP = {}
+        # def demoni(x, amt):
+        #     if (x,amt) in DP:
+        #         return DP[(x,amt)]
 
-            if x == len(coins):
-                return float('inf')
-            if amt == 0 :
-                return 0
-            if amt < 0:
-                return float('inf')
+        #     if x == len(coins):
+        #         return float('inf')
+        #     if amt == 0 :
+        #         return 0
+        #     if amt < 0:
+        #         return float('inf')
 
             
 
-            result = demoni(x+1,amt)
-            if coins[x] <= amt :
-                result = min(result, demoni(x,amt-coins[x])+1)
+        #     result = demoni(x+1,amt)
+        #     if coins[x] <= amt :
+        #         result = min(result, demoni(x,amt-coins[x])+1)
             
-            DP[(x,amt)] = result
-            return result
+        #     DP[(x,amt)] = result
+        #     return result
 
         
 
 
 
-        result = demoni(0, amount)
-        if result == float('inf'):
-            return -1
-        else:
-            return result
+        # result = demoni(0, amount)
+        # if result == float('inf'):
+        #     return -1
+        # else:
+        #     return result
 
 
 
 
 
 
+        # OPTIMAL SOLUTION with 1D DP
+        # BOTTOM UP APPROACH
+        # we build DP[0] TO DP[amount]
+
+        def opt_coins():
+            DP = [float('inf')] * (amount+1)
+            DP[0] = 0
+            for x in range(0,amount+1):
+                for coin in coins:
+                    if coin <= x :
+                        DP[x] = min(DP[x], DP[x-coin] + 1)
+                    
+            if DP[amount] == float('inf'):
+                return -1
+            else :
+                return DP[amount]
+        
+        return opt_coins()
 
 
 
+            
 
 
 
-
-
-        # for i in range(x,len(coins)):
-        #         if (i, amt) in DP:
-        #             return DP[(i, amt)]
-        #         # not take :
-        #         result = demoni(i+1,amt)
-
-        #         if coins[i] <= amt:
-        #             result = min(result, demoni(x,amt-coins[i]) + 1)
-        #         DP[(x, amt)] = result
-        #         return result
-        #     return float('inf')
