843fbfab1a405a4ae0b2967be2f148b5a4ff5a24
Rushikesh Mokashi <161769245+Rushikesh-101@users.noreply.github.com>
2026-01-21 13:03:21 +0530

    Time: 3 ms (64.15%), Space: 20.7 MB (9.54%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,74 +1,30 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        opStack = []
-        prevN = 0
-        valString = ''
-        fVal = 0
-        for n in tokens :
+        
+        def reverse_polish():
+            stack = []
 
-                if len(tokens) == 1:
-                    fVal = int(n)
+            for op in tokens:
+
+                if op == '+' or op == '-' or op == '*' or op =='/':
+                    a = int(stack.pop())
+                    b = int(stack.pop())
+                    res = 0
+                    match op:
+                        case '+':
+                            res = a+b
+                        case '-':
+                            res = b-a
+                        case '*':
+                            res = a*b
+                        case '/':
+                            res = b/a
+                    stack.append(res)
+
+                
             
-                elif n == '+' or n == '-' or n == '*' or n == '/' :
-                    # print("\n entered daddy if: ")
-                    # print("\n this is stack:", opStack)
-                    prevN = opStack.pop()
-                    # if opStack[-1] and fVal != 0:
-                    if opStack and fVal == 0:
-                        # print("\n entered first small if :")
-                        valString = opStack.pop()
-                        valString += " " + n 
-                        valString+= " " + prevN
-                        print("\n printing valstring:", valString)
-
-                        fVal = eval(valString)
-                        valString = ''
-                        print("\n printed fval before :", fVal)
-                        fVal = int(fVal) 
-                        print("\n printed fval afyer :", fVal)
-                        opStack.append(str(fVal))
-                       
-
-
-                    elif opStack :
-                        valString =   " " + opStack[-1]
-                        valString += " " + n
-                        valString += " " + prevN
-                        fVal = eval(valString)
-                        
-                        print("\n printing valstring:", valString)
-                        valString = ''
-                        
-                        opStack.pop()
-                        print("\n printed fval before :", fVal)
-                        fVal = int(fVal) 
-                        print("\n printed fval after :", fVal)
-                        opStack.append(str(fVal))
-                        # print("\nthis is valString: ", valString)
-                        # valString = ''
-                        # print("\n this is stack:", opStack) 
-                        # print("\n this is fVal: ", fVal)
-
-                    # elif not opStack or fVal != 0 :
-                    #     # print("\n this is small else :")
-                    #     valString = " " + str(fVal)
-                    #     valString += " " + n
-                    #     valString += " " + prevN                    
-                    #     fVal = eval(valString)
-
-                    #     opStack.append(fVal)
-                    #     # print("\nthis is valString: ", valString)
-                    #     # valString = ''
-                    #     # print("\n this is stack:", opStack)
-                    #     # print("\n this is fVal: ", fVal)
-
-                    
-                else :
-                    # print("\n this is daddy else: ")
-                    opStack.append(n)
-                    # valString = valString + " " + n
-                    # print("\nthis is valString: ", valString)
-                    # print("\n this is stack:", opStack)
-                    # print("\n this is fVal: ", fVal)
-
-        return fVal
+                else:
+                    stack.append(op)
+                
+            return int(stack.pop())
+        return reverse_polish()
