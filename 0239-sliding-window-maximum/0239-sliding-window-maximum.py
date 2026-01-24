0b866153c04bbb83561fde7f830888ddc2aeb43b
Rushikesh Mokashi <161769245+Rushikesh-101@users.noreply.github.com>
2026-01-24 14:56:47 +0530

    Time: 201 ms (29.74%), Space: 34.7 MB (35.98%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,28 +1,31 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         
-
+        from collections import deque
 
         # keep queue[0] updated as per window 
         # compare i and queue[-1] then add
         # from k-1, start appending to res
-
-
+        def slide():
+            result = []
+            queue = deque()
+            
+            for i in range(len(nums)):
+                
+                while queue and queue[0] < (i-k)+1:
+                    queue.popleft()
+                
+                while queue and nums[queue[-1]] < nums[i]:
+                    queue.pop()
+                queue.append(i)
+            
+                if i >= k-1:
+                    result.append(nums[queue[0]])
+            
+            return result
         
-        res = []
-        que = deque()
-
-        for i in range(len(nums)): # i = indexes of nums : 0 1 2 3
-            
-            if que and que[0] <= i-k : 
-                que.popleft()
-            
-            while que and nums[que[-1]] < nums[i] :
-                que.pop()
-            que.append(i)
-
-            if i - k >= -1 :
-                res.append(nums[que[0]])
-            
-        return res
+        return slide()
+                
+        
+        
 
