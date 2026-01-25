3de73f6e35fc84490978d82ab7bbcf94db563682
Rushikesh Mokashi <161769245+Rushikesh-101@users.noreply.github.com>
2026-01-25 13:37:47 +0530

    Time: 703 ms (67.24%), Space: 74.9 MB (87.1%) - LeetHub

--- 1765-map-of-highest-peak/1765-map-of-highest-peak.py
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.py
@@ -6,26 +6,26 @@ class Solution:
             cols = len(isWater[0])
             neigh = ((0,1),(1,0),(-1,0),(0,-1))
             que = deque()
-            visited = set()
             for r in range(rows):
                 for c in range(cols):
                     if isWater[r][c] == 1:
                         isWater[r][c] = 0
                         que.append((r,c))
-                        visited.add((r,c))
+                    else:
+                        isWater[r][c] = -1 
             while que:               
                 r,c = que.popleft()
                 for a,b in neigh:
                     nrow = r+a
                     ncol = c+b
 
-                    if (nrow,ncol) not in visited and 0<=nrow<rows and 0<=ncol<cols:
+                    if 0<=nrow<rows and 0<=ncol<cols and isWater[nrow][ncol] == -1 :
                         isWater[nrow][ncol] = isWater[r][c]+1
-                        visited.add((nrow,ncol))
                         que.append((nrow,ncol))
             
             return isWater
         
         return height()
                 
+        # In above code set usage is unnecessary, can assign -1 to lands in first parsing.
             
