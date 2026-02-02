8c0ec9422c0cff795bc38a03170040f48ff4ed03
Rushikesh Mokashi <161769245+Rushikesh-101@users.noreply.github.com>
2026-02-02 18:40:58 +0530

    Time: 12 ms (43.89%), Space: 24.3 MB (5.83%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -7,65 +7,93 @@ class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         
         # list[0] will give you head of first linked list 
-        old_array = lists
+        # old_array = lists
 
-        def merge(headA, headB):
-            currA = headA
-            currB = headB
-            dummy_node = ListNode(0)
-            dummy = dummy_node
+        # def merge(headA, headB):
+        #     currA = headA
+        #     currB = headB
+        #     dummy_node = ListNode(0)
+        #     dummy = dummy_node
 
-            while currA or currB :
+        #     while currA or currB :
 
-                if not currA and currB:
-                    dummy.next = currB
-                    dummy = dummy.next 
-                    currB = currB.next
+        #         if not currA and currB:
+        #             dummy.next = currB
+        #             dummy = dummy.next 
+        #             currB = currB.next
 
-                elif not currB and currA: 
-                    dummy.next = currA
-                    dummy = dummy.next
-                    currA = currA.next
+        #         elif not currB and currA: 
+        #             dummy.next = currA
+        #             dummy = dummy.next
+        #             currA = currA.next
                 
-                else:
+        #         else:
 
-                    if currA.val > currB.val :
-                        dummy.next = currB
-                        dummy = dummy.next 
-                        currB = currB.next
+        #             if currA.val > currB.val :
+        #                 dummy.next = currB
+        #                 dummy = dummy.next 
+        #                 currB = currB.next
                     
-                    else :
-                        dummy.next = currA
-                        dummy = dummy.next
-                        currA = currA.next
+        #             else :
+        #                 dummy.next = currA
+        #                 dummy = dummy.next
+        #                 currA = currA.next
 
-            return dummy_node.next
+        #     return dummy_node.next
 
         
-        while len(old_array) > 1 :
+        # while len(old_array) > 1 :
             
-            array_len = len(old_array)
-            new_array = []
+        #     array_len = len(old_array)
+        #     new_array = []
             
-            if array_len % 2 == 0:
+        #     if array_len % 2 == 0:
 
-                for i in range(0, array_len, 2): # even LL in array
-                    new_array.append(merge(old_array[i],old_array[i+1]))
+        #         for i in range(0, array_len, 2): # even LL in array
+        #             new_array.append(merge(old_array[i],old_array[i+1]))
                 
-            else:
+        #     else:
 
-                for i in range(0, array_len-1, 2): # odd LL in array
-                    new_array.append(merge(old_array[i],old_array[i+1]))
-                new_array.append(old_array[array_len-1])
+        #         for i in range(0, array_len-1, 2): # odd LL in array
+        #             new_array.append(merge(old_array[i],old_array[i+1]))
+        #         new_array.append(old_array[array_len-1])
 
 
-            old_array = new_array
+        #     old_array = new_array
             
             
-        if old_array:
-            return old_array[0]
-        else :
-            return None
+        # if old_array:
+        #     return old_array[0]
+        # else :
+        #     return None
         
 
 
+        # HEAP BASED APPROACH
+        def heapMerge():
+            # if not lists or not lists[0]:
+            #     return None
+            hip = []
+            heapq.heapify(hip)
+            res = ListNode(0)
+            curr = res
+
+            # Add all initial node ref into heap
+            for i in lists:
+                if not i:
+                    continue
+                heapq.heappush(hip,(i.val,id(i),i))
+            
+            # Build merged list till heap exists
+            while hip:
+                pop = heapq.heappop(hip)
+                curr.next = ListNode(pop[0])
+                curr = curr.next
+                if pop[2].next:
+                    new = pop[2].next
+                    heapq.heappush(hip,(new.val,id(new),new))
+            
+            return res.next
+        
+        return heapMerge()
+            
