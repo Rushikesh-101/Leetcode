6967a0a4c2c2a8fbaf3210c8583ae2e2aded50fe
Rushikesh Mokashi <161769245+Rushikesh-101@users.noreply.github.com>
2026-02-04 03:05:00 +0530

    Time: 7 ms (8.21%), Space: 19.4 MB (23.57%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -2,32 +2,32 @@ class Solution:
     def reverseWords(self, s: str) -> str:
 
 
-        # HOW TO OPTIMISE THIS ??? 
+        # # HOW TO OPTIMISE THIS ??? 
 
-        def reverse():
-            stack = []
-            word = ''
-            for c in range(len(s)):
-                if s[c] == ' ':
-                    if word:
-                        stack.append(word)
-                        word = ''
-                elif c == len(s)-1 and s[c] != ' ':
-                      word += s[c]
-                      stack.append(word)
-                else:
-                    word += s[c]
+        # def reverse():
+        #     stack = []
+        #     word = ''
+        #     for c in range(len(s)):
+        #         if s[c] == ' ':
+        #             if word:
+        #                 stack.append(word)
+        #                 word = ''
+        #         elif c == len(s)-1 and s[c] != ' ':
+        #               word += s[c]
+        #               stack.append(word)
+        #         else:
+        #             word += s[c]
 
 
-            sentence = ''
-            while stack:
-                sentence += stack.pop()
-                if stack:
-                    sentence += ' '
+        #     sentence = ''
+        #     while stack:
+        #         sentence += stack.pop()
+        #         if stack:
+        #             sentence += ' '
 
-            return sentence
+        #     return sentence
         
-        return reverse()
+        # return reverse()
 
 
         # Possibily optimal approach
