3da8f9a047cb788d5f362a92bb1226ac6298cc2a
Rushikesh Mokashi <161769245+Rushikesh-101@users.noreply.github.com>
2026-02-04 13:10:55 +0530

    Time: 1425 ms (7.32%), Space: 57.4 MB (20.78%) - LeetHub

--- 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.py
+++ 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.py
@@ -9,21 +9,15 @@ class Solution:
         def ratioratio():
             hip = []
             heapq.heapify(hip)
-            tot_gain = 0
-            act_ratio = 0
             # Calculating and pushing all initial gains
             for i in range(len(classes)):
                 gain = calcu(i)
                 heapq.heappush(hip,(-gain,i))
 
-                act_ratio += classes[i][0]/classes[i][1]
-
-
             for _ in range(extraStudents):
                 pop = heapq.heappop(hip)
                 idx = pop[1]
                 gain = pop[0]
-                tot_gain += -gain
                 x,y = classes[idx]
                 x += 1
                 y += 1
@@ -32,9 +26,11 @@ class Solution:
                 new_gain = calcu(idx)
                 heapq.heappush(hip,(-new_gain,idx))
             
-            max_total = (act_ratio+tot_gain)/len(classes)
+            total = 0
+            for i in classes:
+                total += i[0]/i[1]
+            return total/len(classes)
 
-            return max_total
         
         return ratioratio()
 
