6a79298556fd67bb030cc6f3de52711d957e5759
Rushikesh Mokashi <161769245+Rushikesh-101@users.noreply.github.com>
2026-02-08 02:55:45 +0530

    Time: 59 ms (69.9%), Space: 29.6 MB (37.46%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -9,17 +9,11 @@ class Solution:
                 area = ((right-left)) * min(height[right],height[left]) 
                 water = max(water,area)
 
-                if height[left] < height[right]:
+                if height[left] <= height[right]:
                     left += 1
                 elif height[left] > height[right]:
                     right -= 1
-                else:
-                    if height[left+1] < height[right-1]:
-                        right -= 1
-                    elif height[left+1] < height[right-1]:
-                        left += 1
-                    else:
-                        left += 1
+                
             
             return water
         
