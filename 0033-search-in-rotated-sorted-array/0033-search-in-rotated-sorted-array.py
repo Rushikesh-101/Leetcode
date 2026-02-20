f4d03b7c768f77832caecb9a391213e1a06d77b2
Rushikesh Mokashi <161769245+Rushikesh-101@users.noreply.github.com>
2026-02-20 15:51:57 +0530

    Time: 0 ms (100%), Space: 19.6 MB (30.96%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,43 +1,37 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-            
-            # if len(nums) == 1:
-            #     if nums[0] == target:
-            #         return 0
+        # Double Binary Search approach
+        # Binary search for pivot
+        # Divide nums into 2 sorted halves
+        # search for target by BS on both of them
 
-            #     else :
-            #         return -1
+        # Another approach(Optimal)
+        # At each point we check which half is sorted
+        # We check if mid can exist in that sorted half
+        # If yes, we reduce pointers to that half else we reduce them to other half.
 
-            # elif len(nums) == 0:
-            #     return -1
+        def half_mad():
 
-            # else :
+            left = 0
+            right = len(nums)-1
 
+            while left <= right:
+                mid = left + (right-left)//2
+                if nums[mid] == target:
+                    return mid
 
-                left = 0
-                right = len(nums) -1
-                mid = 0 
+                if nums[left] <= nums[mid]:# left half is sorted
+                    if nums[left] <= target < nums[mid] :
+                        right = mid-1
+                    else:
+                        left = mid+1
+                else:# right half is sorted
+                    if nums[mid] < target <= nums[right]:
+                        left = mid+1
+                    else:
+                        right = mid-1
 
-                while left <= right:
-
-                    mid = (left + right)//2
-
-                    if nums[mid] == target :
-                        return mid
+            return -1
+        
+        return half_mad()
                     
-                    elif nums[left] <= nums[mid]:
-                        if nums[left] <= target < nums[mid]:
-                            right = mid -1
-                        else:
-                            left = mid + 1
-
-                            
-
-                    elif nums[mid] <= nums[right]:
-                        if nums[mid] < target <= nums[right]:
-                            left = mid +1
-                        else :
-                            right = mid -1
-                
-                if nums[mid] != target :
-                    return -1
