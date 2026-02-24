0d18cdab2d099f9fbf80056c7a447f92c5d661af
Rushikesh Mokashi <161769245+Rushikesh-101@users.noreply.github.com>
2026-02-25 01:48:22 +0530

    Time: 0 ms (100%), Space: 21.1 MB (77.15%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,7 +1,10 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         
-        res = 0
-        for num in nums:
-            res = res ^ num
-        return res
+        def leftAlone():
+            res = 0
+            for num in nums:
+                res = res ^ num
+            return res
+        
+        return leftAlone()
