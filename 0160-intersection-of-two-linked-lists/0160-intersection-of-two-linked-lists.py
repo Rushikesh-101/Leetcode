7881502af368b15491954909d3c3bbc4421ea3e8
Rushikesh Mokashi <161769245+Rushikesh-101@users.noreply.github.com>
2026-03-06 20:03:11 +0530

    Time: 127 ms (5.45%), Space: 38 MB (69.71%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -7,59 +7,44 @@
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         
-        # Pointer redirection : 
-        # Redirecting both pointers from end of list to start of other list. 
-        # Thus equal distance travelled by both list and at the end they 
-        # either meet at intersection or at end of list
+        l1 = headA
+        l2 = headB
 
+        count1 = 0
+        while l1:
+            count1 += 1
+            l1 = l1.next
 
-        currA = headA
-        currB = headB
-
+        count2 = 0
+        while l2:
+            count2 += 1
+            l2 = l2.next
         
-        # while currA != currB :
-        #     # pointing to other lists head
-
-        #     if currA != None:
-        #         currA = currA.next
-        #     else :
-        #         currA = headB
-              
-        #     if currB != None :
-        #         currB = currB.next
-        #     else :
-        #         currB = headA
-    
-        # return currA
-
-        ctr1 = 0
-        while currA != None:
-            ctr1 += 1
-            currA = currA.next
-
-        ctr2 = 0
-        while currB != None:
-            ctr2 += 1
-            currB = currB.next
         
-        currA = headA
-        currB = headB
-
-        if ctr1 > ctr2:
-            diff = ctr1 - ctr2 
-            while diff != 0:
-                currA = currA.next
-                diff -= 1
-
-        else :
-            diff = ctr2-ctr1
-            while diff != 0:
-                currB = currB.next
-                diff -= 1
-        
-        while currA or currB:
-            if currA == currB:
-                return currA
-            currA = currA.next
-            currB = currB.next
-        return None
+        if count1 > count2:
+            grtr = headA
+            diff = count1 - count2
+            for i in range(diff):
+                grtr = grtr.next
+            l2 = headB
+            while grtr:
+                if grtr == l2:
+                    return grtr
+                grtr = grtr.next
+                l2 = l2.next
+                
+            return None
+            
+        else:
+            grtr = headB
+            diff = count2 - count1
+            for i in range(diff):
+                grtr = grtr.next
+            l1 = headA
+            while grtr:
+                if grtr == l1:
+                    return grtr
+                grtr = grtr.next
+                l1 = l1.next
+            return None
+            
