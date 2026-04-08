458d51e2b7fbad14adfa651895048988363b33f4
Rushikesh Mokashi <161769245+Rushikesh-101@users.noreply.github.com>
2026-04-08 18:28:03 +0530

    Time: 0 ms (100%), Space: 19.5 MB (15.2%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,15 +1,19 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = [[]]
+
+        def recurse(i,stack):
+            nonlocal result
+            if i > len(nums)-1:
+                return
+            
+            else:
+                recurse(i+1,stack[:])    # not taking i'th element
+                stack.append(nums[i])
+                result.append(stack[:])
+                recurse(i+1,stack[:])    # taking i'th elment
         
-        def numberOfSubsets():
-            size = len(nums)
-            res_list = []
-            for sett in range(0,2**size):
-                temp_list = []
-                for j in range(0,size):
-                    if sett & (1 << j):
-                        temp_list.append(nums[j])
-                res_list.append(temp_list)
-            return res_list
-        
-        return numberOfSubsets()
+        stack = []
+        recurse(0,stack)
+
+        return result
